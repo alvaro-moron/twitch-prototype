5eb29438c22f92bd8c42f10a61bfa8221ce7b77f
alvaro moron <alvaro.moron@alebateducation.com>
2025-02-07 02:36:39 +0100

    feat(components): added navbar component and his internal components

---
+++ components/ui/MainButton.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+defineProps({
+  buttonText: String,
+  backgroundColor: String,
+})
+</script>
+<template>
+  <button
+    :style="{ backgroundColor: backgroundColor || 'var(--c-background-button)' }"
+    class="mainButton"
+  >
+    {{ buttonText }}
+  </button>
+</template>
+<style scoped lang="scss">
+.mainButton {
+  cursor: pointer;
+  padding: 0.4em;
+  border-radius: 0.5em;
+  white-space: nowrap;
+}
+</style>
