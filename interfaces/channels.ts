885711ad105d51a792ccc6795242682c9fae20b6
alvaro moron <alvaro.moron@alebateducation.com>
2025-02-09 02:56:26 +0100

    feat(interfaces): interfaces needed to the lateral menu added

---
+++ interfaces/channels.ts
@@ -0,0 +1,14 @@
+export interface Channels {
+  broadcaster_language: string
+  broadcaster_login: string
+  display_name: string
+  game_id: string
+  game_name: string
+  id: string
+  is_live: boolean
+  tag_ids: String[]
+  tags: string[]
+  thumbnail_url: string
+  title: string
+  started_at: Date
+}
