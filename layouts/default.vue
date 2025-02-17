57a67cf1246cad2c02291462c4c016ed485e13ec
alvaro moron <alvaro.moron@alebateducation.com>
2025-02-17 01:15:16 +0100

    feat(layout): default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,7 +3,26 @@ import MainNavbar from '@/components/layouts/navbar/MainNavbar.vue'
 import MainMenu from '@/components/layouts/lateralMenu/MainMenu.vue'
 </script>
 <template>
-  <MainNavbar />
-  <MainMenu />
-  <slot />
+  <section class="layout-container">
+    <MainNavbar />
+    <section class="layout-container__main-content">
+      <MainMenu />
+      <section class="layout-container__page-content">
+        <slot />
+      </section>
+    </section>
+  </section>
 </template>
+<style scoped lang="scss">
+.layout-container {
+  &__main-content {
+    margin-top: var(--d-navbar-height);
+  }
+  &__page-content {
+    padding: 0 0 0 var(--d-lateral-menu-width);
+    @include responsive(48em) {
+      padding-left: var(--d-lateral-menu-width-mobile);
+    }
+  }
+}
+</style>
