6108f61b8f9540bd26fc29b908cc7da0a8339ef7
alvaro moron <alvaro.moron@alebateducation.com>
2025-02-17 01:28:17 +0100

    feat(components): component of the live chat

---
+++ components/layouts/chat/MainChat.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+<template>
+  <section class="chat-container">
+    <p>Soy el chat</p>
+  </section>
+</template>
+<style scoped lang="scss">
+.chat-container {
+  width: var(--d-lateral-chat-width);
+  height: 100%;
+  background-color: var(--c-background-navbar);
+  position: fixed;
+  right: 0;
+}
+</style>
