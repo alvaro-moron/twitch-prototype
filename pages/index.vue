5b3692290e880c8dd6a6b6ca37766161799cd89c
alvaro moron <alvaro.moron@alebateducation.com>
2025-02-18 23:48:41 +0100

    feat(pages): responsive modifications and calling more data

--- pages/index.vue
+++ pages/index.vue
@@ -9,19 +9,20 @@ import { home } from '@/mocks/home.json'
 const api = new connectWithTwitchApi()
 
 const popularStreamsSpa = await api.useGetHttpMethod<arrayStreams>(
-  'helix/streams?first=3&type=live&language=es&sort=viewer_count'
+  'helix/streams?first=6&type=live&language=es&sort=viewer_count'
 )
 
 const popularStreamsEng = await api.useGetHttpMethod<arrayStreams>(
-  'helix/streams?first=3&type=live&language=en&sort=viewer_count'
+  'helix/streams?first=6&type=live&language=en&sort=viewer_count'
 )
 
 const popularCategories = await api.useGetHttpMethod<arrayCategories>(
-  'helix/games/top?first=6&sort=viewer_count'
+  'helix/games/top?first=10&sort=viewer_count'
 )
 </script>
 <template>
   <section class="home-container">
+    <h1 class="home-container__h1">Twitch home</h1>
     <listStreams :title="home.gridOne.title" :stream-data="popularStreamsSpa" />
     <listStreams :title="home.gridTwo.title" :stream-data="popularStreamsEng" />
     <listCategories
@@ -35,5 +36,8 @@ const popularCategories = await api.useGetHttpMethod<arrayCategories>(
 <style scoped lang="scss">
 .home-container {
   padding: 1em 1.5em 0;
+  &__h1 {
+    display: none;
+  }
 }
 </style>
